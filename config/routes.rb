2d6d0f356b97cff4a606552692780cbd99cad236
masayoshi12 <miyabigi1222@gmail.com>
2020-12-26 16:58:18 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
- root to: "messages#index"
- resources :users, only: [:edit, :update]
- resources :rooms, only: [:new, :create]
+  root to: "rooms#index"
+  resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
 #  →resourcesとはすべてのアクションを定義するもの newとクリエイトアクションを定義してあげている。
 end
