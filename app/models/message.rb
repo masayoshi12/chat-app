63ac4d7400ae44c433307ebb193e6de986143c0b
masayoshi12 <miyabigi1222@gmail.com>
2020-12-28 14:31:12 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
-
+  has_one_attached :image
+  # has_one_attachedメソッド 各レコードとファイルを1対1の関係で紐づけるメソッド
   validates :content, presence: true
 end
